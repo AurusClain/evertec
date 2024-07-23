373754420380546292757185128c00aea651d9e5
Kendryck N <kendryck.jno@gmail.com>
2024-07-23 18:59:37 -0500

    wip: initial setup of controllers and migrations
    - Add initial versions of CategoryController, MicrositeController, PaymentSettingController, RoleController, UserController
    - Add initial versions of StoreMicrositeRequest, StoreUserRequest, UpdateMicrositeRequest, UpdateUserRequest
    - Add initial versions of Category, Microsite, PaymentSetting, Role models
    - Add initial version of MicrositeFactory
    - Add initial migrations for roles, categories, payment settings, and microsites

---
+++ app/Http/Controllers/MicrositeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Microsite;
+use App\Http\Requests\StoreMicrositeRequest;
+use App\Http\Requests\UpdateMicrositeRequest;
+
+class MicrositeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMicrositeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Microsite $microsite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Microsite $microsite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMicrositeRequest $request, Microsite $microsite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Microsite $microsite)
+    {
+        //
+    }
+}
